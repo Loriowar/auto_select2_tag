95246a83c97e1c854de6ef84fb86d3d8d8999f7c
Ivan Zabrovskiy <loriowar@gmail.com>
2014-12-01 14:03:49 +0300

    Support of default SearchAdapter
     - add alternative options for initialize select2 without specify SearchAdapter

--- lib/auto_select2_tag/helpers/select2_helper_tags.rb
+++ lib/auto_select2_tag/helpers/select2_helper_tags.rb
@@ -18,7 +18,15 @@ module AutoSelect2Tag
       original_data.merge!('s2options' => select2_options) if select2_options.present?
       search_method = options.delete(:search_method)
       classes = ['auto-ajax-select2', original_classes].compact.join(' ')
-      controller_params = { class_name: select2_searcher }
+      controller_params = {}
+      if select2_searcher.is_a?(Hash)
+        # TODO: metaprogramming can help here
+        controller_params[:default_class_name] = select2_searcher[:class_name] if select2_searcher[:class_name].present?
+        controller_params[:default_text_column] = select2_searcher[:text_column] if select2_searcher[:text_column].present?
+        controller_params[:default_id_column] = select2_searcher[:id_column] if select2_searcher[:id_column].present?
+      else
+        controller_params[:class_name] = select2_searcher
+      end
       if search_method.present?
         controller_params.merge!({ search_method: search_method })
       end
